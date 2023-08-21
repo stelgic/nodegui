7bd62fb014fa51d28811884ecf6fef568870af56
silvadom <dompayments@gmail.com>
2023-08-21 15:58:39 +0400

    emit resize event

--- src/cpp/include/nodegui/QtWidgets/QScrollArea/nscrollarea.hpp
+++ src/cpp/include/nodegui/QtWidgets/QScrollArea/nscrollarea.hpp
@@ -12,8 +12,16 @@ class DLL_EXPORT NScrollArea : public QScrollArea, public NodeWidget {
  public:
   using QScrollArea::QScrollArea;  // inherit all constructors of QScrollArea
 
-  virtual void connectSignalsToEventEmitter() { QABSTRACTSCROLLAREA_SIGNALS }
-
+  virtual void connectSignalsToEventEmitter() { 
+    QABSTRACTSCROLLAREA_SIGNALS 
+    QObject::connect(this, &QScrollArea::resizeEvent, [=](QResizeEvent* event) {
+      Napi::Env env = this->emitOnNode.Env();
+      Napi::HandleScope scope(env);
+      auto eventWrap = QResizeEventWrap::constructor.New(
+        {Napi::External<QResizeEvent>::New(env, event)}); 
+      this->emitOnNode.Call({Napi::String::New(env, "resizeEvent"), eventWrap)});
+  }
+  
   // By-pass `protected` access and expose it.
   void setViewportMargins(int left, int top, int right, int bottom) {
     QScrollArea::setViewportMargins(left, top, right, bottom);
