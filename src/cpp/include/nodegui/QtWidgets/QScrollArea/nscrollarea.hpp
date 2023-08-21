7bfbe54e875ca5adcb7889faee0899e12ad13d64
silvadom <dompayments@gmail.com>
2023-08-21 16:01:37 +0400

    fix build error

--- src/cpp/include/nodegui/QtWidgets/QScrollArea/nscrollarea.hpp
+++ src/cpp/include/nodegui/QtWidgets/QScrollArea/nscrollarea.hpp
@@ -19,9 +19,9 @@ class DLL_EXPORT NScrollArea : public QScrollArea, public NodeWidget {
       Napi::HandleScope scope(env);
       auto eventWrap = QResizeEventWrap::constructor.New(
         {Napi::External<QResizeEvent>::New(env, event)}); 
-      this->emitOnNode.Call({Napi::String::New(env, "resizeEvent"), eventWrap)});
+      this->emitOnNode.Call({Napi::String::New(env, "resizeEvent"), eventWrap});
   }
-  
+
   // By-pass `protected` access and expose it.
   void setViewportMargins(int left, int top, int right, int bottom) {
     QScrollArea::setViewportMargins(left, top, right, bottom);
